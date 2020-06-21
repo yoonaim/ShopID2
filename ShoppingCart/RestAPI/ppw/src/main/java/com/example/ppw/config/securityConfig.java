1869a52fa3bfa54fdd98dbf962873172764c652a
Markolim <54051867+Markolim@users.noreply.github.com>
2020-06-21 22:01:58 +0700

    new update securityConfig.java

--- ShoppingCart/RestAPI/ppw/src/main/java/com/example/ppw/config/securityConfig.java
+++ ShoppingCart/RestAPI/ppw/src/main/java/com/example/ppw/config/securityConfig.java
@@ -44,4 +44,29 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/*/.jpg",
                         "/*/.html",
                         "/*/.css",
-                        "/*/.js")
+                        "/*/.js"
+// importing the File class
+import java.io.File;
+
+class Main {
+  public static void main(String[] args) {
+
+    // create a file object for the current location
+    File file = new File("newFile.txt");
+
+    try {
+
+      // trying to create a file based on the object
+      boolean value = file.createNewFile();
+      if (value) {
+        System.out.println("The new file is created.");
+      }
+      else {
+        System.out.println("The file already exists.");
+      }
+    }
+    catch(Exception e) {
+      e.getStackTrace();
+    }
+  }
+}
