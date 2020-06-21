1d7d52d7a3484bd3764c0fbf25a8e70ec9f0f58b
Markolim <54051867+Markolim@users.noreply.github.com>
2020-06-21 21:51:29 +0700

    Create PpwApplication.java

---
+++ ShoppingCart/RestAPI/ppw/src/main/java/com/example/ppw/PpwApplication.java
@@ -0,0 +1,13 @@
+package com.example.ppw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpwApplication.class, args);
+	}
+
+}
