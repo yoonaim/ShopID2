5036ef44829bced0c9cb80bfc8c8cae7f4d05c18
Fitri <38976894+FitrianiGitana3@users.noreply.github.com>
2020-06-21 22:29:28 +0700

    import library

--- ShoppingCart/RestAPI/ppw/src/main/java/com/example/ppw/PpwApplication.java
+++ ShoppingCart/RestAPI/ppw/src/main/java/com/example/ppw/PpwApplication.java
@@ -4,6 +4,8 @@ package com.example.ppw;
 //add library 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootApplication
 public class PpwApplication {
