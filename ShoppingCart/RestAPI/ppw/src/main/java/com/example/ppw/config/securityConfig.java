b131a4995a83799e7a1b6f19ed50f3e487426eed
Ventina Limbong <ventinabong@gmail.com>
2020-06-21 23:02:25 +0700

    modify something

--- ShoppingCart/RestAPI/ppw/src/main/java/com/example/ppw/config/securityConfig.java
+++ ShoppingCart/RestAPI/ppw/src/main/java/com/example/ppw/config/securityConfig.java
@@ -5,9 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,6 +20,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
         jsr250Enabled = true,
         prePostEnabled = true
 )
+
+//class config for security
 class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
